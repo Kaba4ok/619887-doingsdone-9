462b5af584d979c5b8181616d43a36f59e01d566
Сергей <s.p.prohorov@gmail.com>
2019-04-14 13:30:36 +0300

    Добавляет отступы в разметке

--- index.php
+++ index.php
@@ -126,22 +126,22 @@ $tasks = [
 
                 <table class="tasks">
                     <?php foreach ($tasks as $key => $value): ?>
-                    <?php if((string)$value["status"] === "Нет" || ((string)$value["status"] === "Да" && (int) $show_complete_tasks === 1)): ?>
-                    <tr class="tasks__item task <?php if((string)$value["status"] === "Да"): ?> task--completed <?php endif ?>">
-                        <td class="task__select">
-                            <label class="checkbox task__checkbox">
-                                <input class="checkbox__input visually-hidden task__checkbox" type="checkbox" value="1">
-                                <span class="checkbox__text"><?=$value["task"]; ?></span>
-                            </label>
-                        </td>
+                        <?php if((string)$value["status"] === "Нет" || ((string)$value["status"] === "Да" && (int) $show_complete_tasks === 1)): ?>
+                            <tr class="tasks__item task <?php if((string)$value["status"] === "Да"): ?> task--completed <?php endif ?>">
+                                <td class="task__select">
+                                    <label class="checkbox task__checkbox">
+                                        <input class="checkbox__input visually-hidden task__checkbox" type="checkbox" value="1">
+                                        <span class="checkbox__text"><?=$value["task"]; ?></span>
+                                    </label>
+                                </td>
 
-                        <td class="task__file">
-                            <a class="download-link" href="#">Home.psd</a>
-                        </td>
+                                <td class="task__file">
+                                    <a class="download-link" href="#">Home.psd</a>
+                                </td>
 
-                        <td class="task__date"><?=$value["deadline"]; ?></td>
-                    </tr>
-                    <?php endif ?>
+                                <td class="task__date"><?=$value["deadline"]; ?></td>
+                            </tr>
+                        <?php endif ?>
                     <?php endforeach ?>
                     <?php if((int) $show_complete_tasks === 1): ?>
                     <tr class="tasks__item task task--completed">
