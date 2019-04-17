3f956183a03b9e9277fe14fed2f501f91fb6e625
Сергей <s.p.prohorov@gmail.com>
2019-04-17 11:50:20 +0300

    Разделяет код на блоки и собирает все в одном файле

---
+++ functions.php
@@ -0,0 +1,35 @@
+<?php
+    function getTaskCount ($taskArray, $project) {
+        $count = 0;
+        foreach ($taskArray as $key => $value) {
+            if ((string)$value["category"] === $project) {
+                $count++;
+            };
+        };
+        return $count;
+    };
+
+
+    /**
+     * Подключает шаблон, передает туда данные и возвращает итоговый HTML контент
+     * @param string $name Путь к файлу шаблона относительно папки templates
+     * @param array $data Ассоциативный массив с данными для шаблона
+     * @return string Итоговый HTML
+     */
+    function include_template($name, array $data = []) {
+        $name = 'templates/' . $name;
+        $result = '';
+
+        if (!is_readable($name)) {
+            return $result;
+        }
+
+        ob_start();
+        extract($data);
+        require $name;
+
+        $result = ob_get_clean();
+
+        return $result;
+    }
+?>
